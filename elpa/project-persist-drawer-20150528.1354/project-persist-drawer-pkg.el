873f2c480ad6c5593b5cbaa53eb629690e4dfbb3
Pramukta Kumar <pramukta.kumar@gmail.com>
2015-07-20 11:21:09 -0500

    package updates

---
+++ elpa/project-persist-drawer-20150528.1354/project-persist-drawer-pkg.el
@@ -0,0 +1,5 @@
+(define-package "project-persist-drawer" "20150528.1354" "Use a project drawer with project-persist."
+  '((project-persist "0.3")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
