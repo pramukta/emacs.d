5958b441301195aa189107442d6c4ac3f08bb35c
Pramukta Kumar <pramukta.kumar@gmail.com>
2015-08-06 16:57:17 -0500

    subtle visual customizations

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;;; init.el --- My ever changing emacs initialization script
+
 ;; account for windows
 (if (equal system-type 'windows-nt)
     (progn (setq explicit-shell-file-name
@@ -10,7 +12,7 @@
 
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
@@ -28,11 +30,11 @@
 ;; (setq display-buffer-function 'popwin:display-buffer)
 (push '("^\*helm .+\*$" :regexp t) popwin:special-display-config)
 (add-hook 'helm-after-initialize-hook (lambda ()
-                                          (popwin:display-buffer helm-buffer t)
-                                          (popwin-mode -1)))
+                                        (popwin:display-buffer helm-buffer t)
+                                        (popwin-mode -1)))
 
- ;;  Restore popwin-mode after a Helm session finishes.
- (add-hook 'helm-cleanup-hook (lambda () (popwin-mode 1)))
+;;  Restore popwin-mode after a Helm session finishes.
+(add-hook 'helm-cleanup-hook (lambda () (popwin-mode 1)))
 
 
 ;; configure helm keybindings
@@ -59,6 +61,18 @@
 ;; hc-zenburn theme
 (load-theme 'hc-zenburn t)
 
+(defun zencolor (colorname)
+  "Convert hc-zenburn color symbol to hex string"
+  (cdr (assoc (symbol-name colorname) hc-zenburn-colors-alist))
+  )
+
+(print hc-zenburn-colors-alist)
+
+;; visual customizations
+(set-face-attribute 'mode-line nil :box `(:line-width -1 :color ,(zencolor 'hc-zenburn-bg+1) :style nil))
+(set-face-attribute 'mode-line-inactive nil
+                    :box `(:line-width -1 :color ,(zencolor 'hc-zenburn-bg+1) :style nil))
+
 ;; Javascript customizations
 (setq js-indent-level 2)
 ;; Python customizations
@@ -79,3 +93,103 @@
 
 (add-to-list 'company-backends 'company-anaconda)
 (add-hook 'python-mode-hook 'anaconda-mode)
+
+;; Confluence Integration
+(require 'confluence)
+(setq confluence-url "https://timbr-io.atlassian.net/wiki/rpc/xmlrpc")
+
+;; open confluence page
+(global-set-key (kbd "C-x w f") 'confluence-get-page)
+
+;; setup confluence mode
+(add-hook 'confluence-mode-hook
+          (lambda()
+            (local-set-key (kbd "C-x w") 'confluence-prefix-map)))
+
+(add-hook 'confluence-mode-hook
+          (lambda()
+            (visual-line-mode t)))
+
+(add-hook 'confluence-mode-hook
+          (lambda()
+            (global-set-key (kbd "C-c C-r") (lambda()
+                                              (mark-whole-buffer)
+                                              (confluence-xml-reformat)))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" default)))
+ '(sml/mode-width
+   (if
+       (eq powerline-default-separator
+           (quote arrow))
+       (quote right)
+     (quote full)))
+ '(sml/pos-id-separator
+   (quote
+    (""
+     (:propertize " " face powerline-active1)
+     (:eval
+      (propertize " "
+                  (quote display)
+                  (funcall
+                   (intern
+                    (format "powerline-%s-%s" powerline-default-separator
+                            (car powerline-default-separator-dir)))
+                   (quote powerline-active1)
+                   (quote powerline-active2))))
+     (:propertize " " face powerline-active2))))
+ '(sml/pos-minor-modes-separator
+   (quote
+    (""
+     (:propertize " " face powerline-active1)
+     (:eval
+      (propertize " "
+                  (quote display)
+                  (funcall
+                   (intern
+                    (format "powerline-%s-%s" powerline-default-separator
+                            (cdr powerline-default-separator-dir)))
+                   (quote powerline-active1)
+                   nil)))
+     (:propertize " " face sml/global))))
+ '(sml/pre-id-separator
+   (quote
+    (""
+     (:propertize " " face sml/global)
+     (:eval
+      (propertize " "
+                  (quote display)
+                  (funcall
+                   (intern
+                    (format "powerline-%s-%s" powerline-default-separator
+                            (car powerline-default-separator-dir)))
+                   nil
+                   (quote powerline-active1))))
+     (:propertize " " face powerline-active1))))
+ '(sml/pre-minor-modes-separator
+   (quote
+    (""
+     (:propertize " " face powerline-active2)
+     (:eval
+      (propertize " "
+                  (quote display)
+                  (funcall
+                   (intern
+                    (format "powerline-%s-%s" powerline-default-separator
+                            (cdr powerline-default-separator-dir)))
+                   (quote powerline-active2)
+                   (quote powerline-active1))))
+     (:propertize " " face powerline-active1))))
+ '(sml/pre-modes-separator (propertize " " (quote face) (quote sml/modes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
